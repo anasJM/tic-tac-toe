35b952279d7780e1aa1b0a09bde4f0cb6c474ff3
anasJM <anasjamlaoui@gmail.com>
2026-02-08 17:53:15 +0100

    - checking the winner and draw - display the the result of the game - fixing bug: the player can't click a filled cell

--- script.js
+++ script.js
@@ -12,7 +12,11 @@ function Cell() {
         // console.log("yes")
     }
 
-    return { getCellValue, setCellValue };
+    const setCellValueToZero = () => {
+        cellValue = 0;
+    }
+
+    return { getCellValue, setCellValue, setCellValueToZero };
 }
 
 
@@ -36,8 +40,22 @@ function Gameboard() {
         }
     }
 
+    // reset the board
+    const resetBoard = () => {
+        board.forEach(element => {
+            console.log("row")
+            element.forEach(value => {
+                value.setCellValueToZero();
+                console.log(value.getCellValue())
+            });
+        });
+
+        console.log("form reset board methoooooooooood!")
+        // console.table(board);
+    }
+
     // select the cell 
-    const selectCell = (row, col, player) => {
+    const selectCell = (row, col, player) => {  
         board[row][col].setCellValue(player);
     }
 
@@ -47,7 +65,7 @@ function Gameboard() {
         return boardCellValues;
     }
 
-    return { getBoard, selectCell, printBoard };
+    return { getBoard, selectCell, printBoard, resetBoard};
 }
 
 
@@ -72,10 +90,18 @@ function GameController(
         }
     ];
 
+    // inisialize winner object
+    let winner = {};
+
     let activePlayer = players[0];
+
+    // getter winner
+    const getWinner = () => winner;
     
+    // getter active player
     const getActivePlayer = () => activePlayer;
 
+    // switch player turn method
     const switchPlayerTurn = () => {
         if (activePlayer === players[1]) {
             activePlayer = players[0];
@@ -89,17 +115,80 @@ function GameController(
         console.log(`${getActivePlayer().name}'s turn.`);
     }
 
+    // play round method
     const playRound = (row, col) => {
-        console.log(`${activePlayer.name} shoose a cell (row: ${row+1}, column: ${col+1})`);
-        board.selectCell(row, col, getActivePlayer());
+        if (board.getBoard()[row][col].getCellValue() === 0 && Object.keys(winner).length === 0) {
+            board.selectCell(row, col, getActivePlayer());
+            switchPlayerTurn();
+            printNewRound();
+            checkWinner();
+        }
+    }
 
-        switchPlayerTurn();
-        printNewRound();
+    // play again and reset the board
+    const resetGame = () => {
+        board.resetBoard();
+        winner = {};
+        activePlayer = players[0];
+        resetButton.style.display = "none";
+        console.log("game reset!");
+    }
+
+    // cheking who's the winner winner chicken dinner
+    const checkWinner = () => {
+        let checkboard = board.getBoard();
+
+        let row0col0 = checkboard[0][0].getCellValue();
+        let row0col1 = checkboard[0][1].getCellValue();
+        let row0col2 = checkboard[0][2].getCellValue();
+        let row1col0 = checkboard[1][0].getCellValue();
+        let row1col1 = checkboard[1][1].getCellValue();
+        let row1col2 = checkboard[1][2].getCellValue();
+        let row2col0 = checkboard[2][0].getCellValue();
+        let row2col1 = checkboard[2][1].getCellValue();
+        let row2col2 = checkboard[2][2].getCellValue();
+
+        if (row0col0 === row0col1 && row0col1 === row0col2 && row0col0 !== 0) {
+            console.log(winner);
+            winner = activePlayer;
+        } else if (row1col0 === row1col1 && row1col1 === row1col2 && row1col0 !== 0) {
+            switchPlayerTurn()
+            winner = activePlayer;
+            console.log(winner);
+        } else if (row2col0 === row2col1 && row2col1 === row2col2 && row2col0 !== 0) {
+            switchPlayerTurn()
+            winner = activePlayer;
+            console.log(winner);
+        } else if (row0col0 === row1col0 && row1col0 === row2col0 && row0col0 !== 0) {
+            switchPlayerTurn()
+            winner = activePlayer;
+            console.log(winner);
+        } else if (row0col1 === row1col1 && row1col1 === row2col1 && row0col1 !== 0) {
+            switchPlayerTurn()
+            winner = activePlayer;
+            console.log(winner);
+        } else if (row0col2 === row1col2 && row1col2 === row2col2 && row0col2 !== 0) {
+            switchPlayerTurn()
+            winner = activePlayer;
+            console.log(winner);
+        } else if (row0col0 === row1col1 && row1col1 === row2col2 && row0col0 !== 0) {
+            switchPlayerTurn()
+            winner = activePlayer;
+            console.log(winner);
+        } else if (row0col2 === row1col1 && row1col1 === row2col0 && row0col2 !== 0) {
+            switchPlayerTurn()
+            winner = activePlayer;
+            console.log(winner);
+        } else if (row0col0 != 0 && row0col1 != 0 && row0col2 != 0 && row1col0 != 0 && row1col1 != 0 && row1col2 != 0 && row2col0 != 0 && row2col1 != 0 && row2col2 != 0) {
+            winner = {draw: "draw"}
+            console.log("draaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaw!!")
+            resetButton.style.display = "block";
+        }
     }
 
     printNewRound();
 
-    return { getActivePlayer, playRound, board }
+    return { getActivePlayer, playRound, resetGame, getWinner, board }
 }
 
 
@@ -110,6 +199,12 @@ function GameController(
 const turnHeader = document.querySelector(".turn");
 const boardSection = document.querySelector(".board");
 
+// get all the buttons
+const cells = document.querySelectorAll(".cell");
+
+// get the reset buttons
+const resetButton = document.querySelector(".reset-button");
+
 
 
 
@@ -118,28 +213,47 @@ const boardSection = document.querySelector(".board");
 // screen controller
 function screenController() {
     const game = GameController();
-    let activePlayer = game.getActivePlayer().name;
-    let board = game.board.getBoard();
 
     //update screen method
     const updateScreen = () => {
-        // show the active player 
-        turnHeader.textContent = `${activePlayer}'s turn!`;
+        // get current player
+        let activePlayer = game.getActivePlayer().name;
+        let board = game.board.getBoard();
 
-        boardSection.innerHTML = "";
+        // display the turn and the winner and if it's draw
+        if (game.getWinner().draw) {
+            turnHeader.textContent = "sheeeeeeesh! it's a tie."
+        } else if (game.getWinner().name) {
+            turnHeader.textContent = `${game.getWinner().name} wins!`;
+            resetButton.style.display = "block";
+        } else {
+            // show the active player 
+            turnHeader.textContent = `${activePlayer}'s turn!`;
+        }
+
+        // boardSection.innerHTML = "";
 
         //render the cells
         let row = 1;
+
         board.forEach(element => {
             let column = 1;
             element.forEach(value => {
-                boardSection.innerHTML += `
-                    <button class="cell" data-row="${row}" data-column="${column}"></button>
-                `;
+                
+                const button = document.querySelector(`[data-row="${row}"][data-column="${column}"]`)
+                if (button) {
+                    if (value.getCellValue() === 1) {
+                        button.textContent = "X";
+                    } else if(value.getCellValue() === 2) {
+                        button.textContent = "O";
+                    } else {
+                        button.textContent = "";
+                    }
+                }
+                
                 column++;
             });
             row++;
-
         });
     }
 
@@ -148,22 +262,24 @@ function screenController() {
         // update the board
         updateScreen();
 
-        // get all the buttons
-        const cells = document.querySelectorAll(".cell");
-
         cells.forEach(element => {
+            // updateScreen();
             element.addEventListener("click", (event) => {
-                // switch player turn
                 rowCell = event.target.dataset.row -1
                 colCell = event.target.dataset.column -1
 
                 // playround
                 game.playRound(rowCell, colCell)
-
+                updateScreen()
                 console.table(game.board.printBoard())
             })
         });
     }
+
+    resetButton.addEventListener("click", (e) => {
+        game.resetGame();
+        updateScreen();
+    })
 
     return { updateScreen, clickHandlerBoard }
 }
